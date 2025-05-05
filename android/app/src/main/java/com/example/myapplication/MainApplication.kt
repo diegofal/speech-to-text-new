e7061377c113a1bb1f15b2caa7c3116b5ae0dfd9
Diego Falciola <diego.falciola@gmail.com>
2025-05-05 12:23:34 -0300

    Remove speech recognition implementation and update Android build configs

--- android/app/src/main/java/com/example/myapplication/MainApplication.kt
+++ android/app/src/main/java/com/example/myapplication/MainApplication.kt
@@ -4,11 +4,10 @@ import android.app.Application
 import android.util.Log
 
 class MainApplication : Application() {
-    private val TAG = "SpeechRecognitionApp"
+    private val TAG = "SpeechToTextApp"
 
     override fun onCreate() {
         super.onCreate()
         Log.d(TAG, "Application started")
     }
-
 }
